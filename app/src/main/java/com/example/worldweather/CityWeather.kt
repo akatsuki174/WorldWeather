68b2b4e2dfb76f2d2b64bd74d2338e0ffe53e861
akatsuki174 <dev2015js@gmail.com>
2019-06-26 13:50:47 +0900

    gsonの使い方が間違っている気がしたので修正

--- app/src/main/java/com/example/worldweather/CityWeather.kt
+++ app/src/main/java/com/example/worldweather/CityWeather.kt
@@ -2,9 +2,14 @@ package com.example.worldweather
 
 import com.google.gson.annotations.SerializedName
 
-data class CityWeather (
+data class CityWeather(
     @SerializedName("name")
     val name: String,
+    @SerializedName("weather")
+    val weather: List<Weather>
+)
+
+data class Weather(
     @SerializedName("description")
     val description: String
 )
